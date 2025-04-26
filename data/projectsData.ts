540caf1f42403a6b72240585b3f9ec861d2723dc
Mark Williams <78853903+markxwilliams@users.noreply.github.com>
2025-04-26 21:22:20 +1000

    Add files via upload

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Secure Messaging Apps',
+    description: `A comparison of secure messaging apps from a security and privacy point of view.`,
+    imgSrc: '/static/images/securemess.png',
+    href: 'https://securemessagingapps.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'CIAM Blog',
+    description: `A blog about customer identity and access management.`,
+    imgSrc: '/static/images/ciam.png',
+    href: 'https://ciamblog.net',
   },
 ]
 
