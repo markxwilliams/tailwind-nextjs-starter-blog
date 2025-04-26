4cbcde6207f62d9ac20de94e7889390d5f502346
Mark Williams <78853903+markxwilliams@users.noreply.github.com>
2025-04-26 22:02:30 +1000

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -35,11 +35,12 @@ export default function Home({ posts }) {
                     <div className="space-y-5 xl:col-span-3">
                       <div className="space-y-6">
                         <div>
-                          <h2 className="text-2xl leading-8 font-bold tracking-tight md:text-xl md:leading-7">
-                            <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                          {title}
-                      </Link>
-                        </h2>
+                          h2 className="text-2xl leading-8 font-bold tracking-tight">
+                            <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
                           <div className="flex flex-wrap">
                             {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
