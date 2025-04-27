6648b6d0f5c9e8bae0ae9c564ad20823cda4ea97
Mark Williams <78853903+markxwilliams@users.noreply.github.com>
2025-04-27 17:17:03 +1000

    Update Main.tsx

--- app/Main.tsx
+++ app/Main.tsx
@@ -38,8 +38,7 @@ export default function Home({ posts }) {
                            <h2 className="text-2xl leading-8 font-bold tracking-tight">
                             <Link
                               href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
+                              className="text-gray-900 dark:text-gray-100">
                               {title}
                             </Link>
                           </h2>
