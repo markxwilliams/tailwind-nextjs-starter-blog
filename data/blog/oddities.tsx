20ed43ce3d168c75bd4a89568636ce29476a9fef
Mark Williams <78853903+markxwilliams@users.noreply.github.com>
2025-05-03 14:55:52 +1000

    Create oddities.tsx

---
+++ data/blog/oddities.tsx
@@ -0,0 +1,37 @@
+---
+title: 'Oddities in the Australian Cyber Security Industry'
+date: '2025-05-03'
+lastmod: '2025-05-03'
+tags: ['cyber security']
+draft: false
+summary: 'Oddities in the Australian Cyber Security Industry'
+images: ['/static/images/twitter-card.png']
+---
+
+## Introduction
+
+I've worked in the Australian cyber security industry since 2016. It's not without its oddities. 
+
+## "Australian Experience" 
+
+When I first moved to Australia, multiple recruiters asked me the same question: "Do you have experience in Australia?". Despite having then 11 years' expeience in cyber security -- in New Zealand, the UK, and Germany -- this was a question that I was seriously asked. 
+
+What makes Australia special such that one requires "Australian experience"? Nothing. Business culture is similar to New Zealand. In the UK there's a lot more complaining and drinking after work. In Germany... well, that's a topic for another day.
+
+Unsurprisingly, nine years later, I've seen nothing significantly different Australia's business culture compared to the other countries in which I've worked. 
+
+## The ACSC
+
+The [Australian Cyber Security Centre](https://www.cyber.gov.au), brought you you by the ASD... a [spy agency](https://www.asd.gov.au/about/accountability-governance/publications/asd-corporate-plan-2021-22/asds-purpose). Notoriously, Edward Snowden showed the ASD's [involvement](https://www.crikey.com.au/2019/07/09/australian-signals-directorate-book/) in [mass](https://www.smh.com.au/politics/federal/edward-snowden-documents-reveal-indonesian-phone-networks-penetrated-by-australian-spies-20140216-32tyu.html) [surveillance](https://www.abc.net.au/news/2014-05-14/documents-reveal-new-details-in-australias-bid-for-nsa-spy-help/5453764).
+
+However, despite the ASD's [clear conflict of interest](https://www.schneier.com/essays/archives/2009/03/who_should_be_in_cha.html), the ACSC's [Essential 8](https://www.cyber.gov.au/resources-business-and-government/essential-cybersecurity/essential-eight) is followed religiously by many Australian organisaitons. The Essential 8 is cyber security 101; it's nothing you can't find in any cyber security textbook, course, or sprawled across the Internet. 
+                                                                                                                                              
+Despite beiing part of a spy agency, the ACSC is rarely, if ever, criticised by cyber security professionals.                                                                                                                                                          
+                                                                                                                                              
+## "I'm a CISO"
+
+No, you're not a CISO. You don't have the word "Chief" in your title, and you still report to someone who reports to the CIO. Australian cyber security managers' egos continue to amaze me. 
+                                                                                                                                             
+
+
+
