83313c457ea1c2bff9d1b18298e82d38eee7b2cd
Mark Williams <78853903+markxwilliams@users.noreply.github.com>
2025-05-23 12:51:40 +1000

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -29,17 +29,18 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    //umamiAnalytics: {
+    umamiAnalytics: {
       // We use an env variable for this site to avoid other users cloning our analytics ID
-      //umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      umamiWebsiteId: process.env.b52ab0eb-0ce9-4d62-a7d5-dcc9e4b23e2d, // e.g. 123e4567-e89b-12d3-a456-426614174000
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
+      src: 'https://williams.ms/send/js/script.js'
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
     //},
     plausibleAnalytics: {
        plausibleDataDomain: 'williams.ms', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // If you are hosting your own Plausible.
-    src: 'https://williams.ms/js/script.js', // e.g. https://williams.ms/js/script.js
+    src: 'https://williams.ms/public/js/script.js', // e.g. https://williams.ms/js/script.js
     },
     // simpleAnalytics: {},
     // posthogAnalytics: {
