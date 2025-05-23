25e4e22a275fd3bf62c65e44dc89a103cb1e8419
Mark Williams <78853903+markxwilliams@users.noreply.github.com>
2025-05-23 12:52:44 +1000

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -34,9 +34,9 @@ const siteMetadata = {
       umamiWebsiteId: process.env.b52ab0eb-0ce9-4d62-a7d5-dcc9e4b23e2d, // e.g. 123e4567-e89b-12d3-a456-426614174000
       // You may also need to overwrite the script if you're storing data in the US - ex:
       // src: 'https://us.umami.is/script.js'
-      src: 'https://williams.ms/send/js/script.js'
+      src: 'https://williams.ms/send/js/script.js',
       // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-    //},
+    },
     plausibleAnalytics: {
        plausibleDataDomain: 'williams.ms', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // If you are hosting your own Plausible.
